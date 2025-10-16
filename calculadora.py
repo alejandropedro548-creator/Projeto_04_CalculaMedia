126135deb327581a5c6d5e8b5a8d663ffedd5b54
Pedro Alejandro Lima Dos Anjos <alejandropedro548@gmail.com>
2025-10-16 13:34:51 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+nota1 = float(input("Digite sua nota no 1° Bimestre"))
+nota2 = float(input("Digite sua nota no 2° Bimestre"))
+nota3 = float(input("Digite sua nota no 3° Bimestre"))
+nota4 = float(input("Digite sua nota no 4° Bimestre"))
+
+total = nota1 + nota2 + nota3 + nota4
+
+media = total / 4
+
+print(media)
+
+
